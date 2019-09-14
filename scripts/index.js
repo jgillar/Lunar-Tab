76cf5a6c7bdf10c3df3026f6c963ae591f03591f
jgillar <jamie.gillar@gmail.com>
2019-09-13 23:10:54 -0400

    added a "fix" for the border around the moon generated by js-planet-phase

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
 
+    drawPlanetPhase(document.getElementById("moon-container"), 0.23, true, {diameter:500, earthshine:0, shadowColour: "#0c0119", lightColour: "#c2b9d8", blur:0})
+
+    
 });
