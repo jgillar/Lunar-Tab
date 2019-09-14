3779badb9d69fdc44facfd3b1165ba7d0812b60b
jgillar <jamie.gillar@gmail.com>
2019-09-14 07:47:52 -0400

    improved the border fix for the moon

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
-    drawPlanetPhase(document.getElementById("moon-container"), 0.23, true, {diameter:500, earthshine:0, shadowColour: "#0c0119", lightColour: "#c2b9d8", blur:0})
+    drawPlanetPhase(
+        document.getElementById("moon"), 0.33, false, 
+        {
+            diameter:500, 
+            earthshine:0, 
+            shadowColour: "#1e132c", 
+            lightColour: "#c2b9d8", 
+            blur:6
+        });
 
-    
+     
 });
