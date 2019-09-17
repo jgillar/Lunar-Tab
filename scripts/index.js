8b0363fffa309b46fc062720850c9915cb58c0f6
jgillar <jamie.gillar@gmail.com>
2019-09-16 23:17:13 -0400

    got rid of jquery since it's unnecessary planetary hour calculation now works

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,6 @@ window.addEventListener("load", function () {
     document.getElementById("location-form").addEventListener("submit", function (event) {
         let date = new Date();
         date.setDate(date.getDate() + 5);
-        console.log("clicky")
-        console.dir(date);
         event.preventDefault();
         geocoder.send(document.getElementById("location-textbox").value);
         populateMoonStats(date);
@@ -15,15 +13,69 @@ window.addEventListener("load", function () {
         return "" + time.getHours() % 12 + ":" + time.getMinutes() + " " + (time.getHours() > 12 ? "PM" : "AM");
     };
 
+    //get the current planetary hour
+    //returns an object containing the name of the 'ruler' of the hour and interval
+    let getPlanetaryHour = (date) => {
+        let sun = SunCalc.getTimes(date, window.localStorage.latitude, window.localStorage.longitude),
+            moon = SunCalc.getMoonTimes(date, window.localStorage.latitude, window.localStorage.longitude),
+            chaldeanSequence = [6, 4, 2, 0, 5, 3, 1], //Saturn, Jupiter, etc. 
+            weekSequence = ["Sun", "Moon", "Mars", "Mercury", "Jupiter", "Venus", "Saturn"],
+            dayOfTheWeek = date.getDay(),
+            currentTime = date.getTime(),
+            dayLength,
+            nightLength,
+            dayOfTheWeekIndex,
+            chaldeanChunk,
+            hourLength,
+            hourNumber,
+            planetaryHour = {};
+
+        //the Chaldean Sequence repeats every day starting with the day's 'ruling planet'
+        //shift the chaldean sequence array to start with the day's 'ruler'
+        dayOfTheWeekIndex = chaldeanSequence.indexOf(dayOfTheWeek);
+        chaldeanChunk = chaldeanSequence.splice(0, dayOfTheWeekIndex);
+        chaldeanSequence.push(...chaldeanChunk);
+
+        //there are 12 'planetary hours' during the day and 12 at night
+        dayLength = Math.abs(sun.sunrise.getTime() - sun.sunset.getTime());
+        nightLength = Math.abs(moon.rise.getTime() - moon.set.getTime());
+
+        //there are 12 equal length hours
+        //to find which one the current hour sits in we just need to divide by
+        //the length of the hour length 
+        if (currentTime < moon.rise.getTime()) {
+            hourLength = dayLength / 12;
+            hourNumber = Math.floor((currentTime - sun.sunrise.getTime()) / hourLength);
+            planetaryHour.hourStart = sun.sunrise.getTime() + hourNumber * hourLength;
+            planetaryHour.hourEnd = planetaryHour.hourStart + hourLength;
+        }
+        else {
+            hourLength = nightLength / 12;
+            hourNumber = Math.floor((currentTime - moon.rise.getTime()) / hourLength);
+            planetaryHour.hourStart = moon.rise.getTime() + hourNumber * hourLength;
+            planetaryHour.hourEnd = planetaryHour.hourStart + hourLength;
+        }
+        planetaryHour.name = weekSequence[chaldeanSequence[hourNumber % 7]];
+        console.log(planetaryHour);
+
+        // for (let x = 0; x < 24; x++) {
+        //     if (x < 12)
+        //         console.log(x, weekSequence[chaldeanSequence[x % 7]], new Date(sun.sunrise.getTime() + x * hourLength));
+        //     else
+        //         console.log(x, weekSequence[chaldeanSequence[x % 7]], new Date(moon.rise.getTime() + x * hourLength));
+        // }
+    }
+
+    getPlanetaryHour(new Date());
 
     //get the astrological sign
     let getAstroSign = (date) => {
         //the moon's 'sign' is dependent on its ecliptic longitude
         //I couldn't find any algorithims or formulas that weren't part of
-        //a graduate level astronomy course lecture so I'm using some random library
+        //a graduate level astronomy course lecture so I'm using a library
         //the library incorrectly calculates the constellation so it's done here
         let elongitude = MoonCalc.datasForDay(date).ecliptic.longitude;
-        if(elongitude < 29)
+        if (elongitude < 29)
             return "Aries";
         else if (elongitude < 59)
             return "Taurus";
@@ -45,25 +97,10 @@ window.addEventListener("load", function () {
             return "Capricorn";
         else if (elongitude < 329)
             return "Aquarius";
-        else 
+        else
             return "Pisces";
     }
 
-/* 
-    var x = 0;
-    while(x < 10){
-        var date = new Date();
-    
-        date.setDate( date.getDate() + x);
-    
-        let data = MoonCalc.datasForDay(date);
-        console.log(date);
-        console.log(data.ecliptic.longitude);
-        console.log(getAstroSign(date));
-    
-    x++;
-    }
-     */
     //get the name of the phase
     let getPhaseName = (phase) => {
         if (phase === 0)
@@ -87,8 +124,8 @@ window.addEventListener("load", function () {
     // populates the moonrise, moonset, etc. containers
     let populateMoonStats = (date) => {
         let times,
-        illumination,
-        astroSign;
+            illumination,
+            astroSign;
 
         times = SunCalc.getMoonTimes(date, window.localStorage.latitude, window.localStorage.longitude);
         document.querySelector("#box-moonrise span:nth-child(2)").innerHTML = timeToString(times.rise);
@@ -97,7 +134,7 @@ window.addEventListener("load", function () {
         illumination = SunCalc.getMoonIllumination(date);
         document.querySelector("#box-phase span:first-child").innerHTML = getPhaseName(illumination.phase);
         document.querySelector("#box-phase span:nth-child(2)").innerHTML = Math.round(illumination.fraction * 100) + "%";
-        
+
         astroSign = getAstroSign(date);
         document.querySelector("#box-zodiac span:nth-child(2)").innerHTML = astroSign + ", " + Math.round(0) + "&#176;";
         document.querySelector("#box-zodiac img").src = "svg/" + astroSign + ".svg#svgView(viewBox(-4,-4,24,24))";
@@ -114,8 +151,6 @@ window.addEventListener("load", function () {
 
     populateMoonStats(new Date());
 
-
-
     /* just a simple object for handling API calls
         @buildQuery  builds up a query string 
         @send        ajax request to the api, returns null on error or long/lat pair on success
@@ -128,12 +163,11 @@ window.addEventListener("load", function () {
         },
         send: function (value) {
             let request = new XMLHttpRequest();
-
             //successful request 
             request.addEventListener("load", (event) => {
                 if (event.target.status === 200) {
-                    let responseObj = JSON.parse(event.target.responseText);
-                    let localStorage = window.localStorage;
+                    let responseObj = JSON.parse(event.target.responseText),
+                        localStorage = window.localStorage;
                     //store the long/lat coords and location name for quick access
                     console.log("localstorage");
                     localStorage.setItem("longitude", responseObj.results[0].geometry.location.lng);
